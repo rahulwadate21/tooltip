1c0e44c12bacd152ef1a177d91ae57dba416fc0b
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-05-09 13:04:35 +0530

    build js file

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="tooltip">
-      Hover over me!
-      <span class="tooltiptext">Thanks for hovering! I'm a tooltip</span>
-    </div>
+    
   );
 }
 
